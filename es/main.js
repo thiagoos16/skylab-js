08d94317e26e1479fe1ef9e03cdc6b6398897ba2
thiagoos16 <tos@icomp.ufam.edu.br>
2020-02-26 14:16:37 -0400

    working with heranca

--- es/main.js
+++ es/main.js
@@ -1,16 +1,30 @@
-class TodoList {
+class List {
     constructor() {
         this.todos = [];
     }
 
-    addTodo() {
-        this.todos.push('new item');
+    add(data) {
+        this.todos.push(data);
         console.log(this.todos);
+    }
+}
+
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.usuario = 'thiago';
+    }
+
+    showUser() {
+        console.log(this.usuario);
     }
 }
 
 const MyList = new TodoList();
 
 document.querySelector('#add').onclick = function () {
-    MyList.addTodo();
+    MyList.add("new item");
 }
+
+MyList.showUser();
