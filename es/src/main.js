ae16bef0787df4535613fd77b46bafd756593201
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2020-03-04 15:30:34 -0400

    add webpack server dependencies

--- es/src/main.js
+++ es/src/main.js
@@ -1,6 +1,6 @@
 // import soma as somaNatural, {sub} from './functions';
 // import * as functions from './functions';
-import soma from './soma';
+import soma from '../soma';
 
 // console.log(somaNatural(1, 2));
 // console.log(functions.soma(1,2));
