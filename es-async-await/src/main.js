090597385e67fddc6cc82710f7995d539a35224f
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2020-03-06 14:59:33 -0400

    add async examples

--- es-async-await/src/main.js
+++ es-async-await/src/main.js
@@ -1,12 +1,59 @@
-const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+import axios from 'axios';
+
+//begin
+const delay = time => new Promise(resolve => setTimeout(() => {resolve('ok')}, 1000));
 
 async function umPorSegundo() {
-    console.log(await delay());
-    console.log('1s');
-    console.log(await delay());
+    await delay('1s');
     console.log('2s');
-    console.log(await delay());
-    console.log('3s');
+    await delay('3s');
+    console.log('4s');
+    await delay('5s');
+    console.log('6s');
 }
 
-umPorSegundo();
+//umPorSegundo();
+//end
+
+//begin
+async function getUserFromGithub(user) {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${user}`);  
+        console.log(response);
+    } catch (error) {
+        console.warn('Usuário não existe');
+    }
+}
+
+/*getUserFromGithub('diego3g');
+getUserFromGithub('diego3g124123');*/
+//end
+
+//begin
+class Github {
+    static async getRepositories(repo) {
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${repo}`);
+            console.log(response.data);
+        } catch (error) {
+            console.warn('Repositório não existe');
+        }
+    }
+}
+
+/*Github.getRepositories('rocketseat/rocketseat.com.br');
+Github.getRepositories('rocketseat/dslkvmskv');*/
+//end
+
+//begin
+const buscaUsuario = async usuario => {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${user}`);
+        console.log(response.data);
+    } catch (error) {
+        console.warn('Usuário não existe');
+    }
+}
+
+buscaUsuario('diego3g');
+//end
