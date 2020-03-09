62512c7c5140753d70b97797c9297c362bfa28b3
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2020-03-09 16:29:52 -0400

    add get data from api

--- github-repositories/src/main.js
+++ github-repositories/src/main.js
@@ -1,9 +1,12 @@
+import api from './api';
+
 class App {
     constructor() {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
         this.listEl = document.getElementById('repo-list');
+        this.inputEl = document.querySelector('input[name=repository]');
 
         this.registerHandlers();
     }
@@ -12,17 +15,48 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event) {
-        event.preventDefault();
-        
-        this.repositories.push({
-            name: 'Rocketseat.com.br',
-            description: 'Tire a sua ideia do papel e dê vida a sua startup.',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url: 'http://github.com/rockseat/rocketseat.com.br'
-        });
+    setLoading(loading = true) {
+        if (loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id', 'loading');
 
-       this.renderRepositories();
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event) {
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0)
+            return;
+
+        this.setLoading();
+
+        try {
+            const response = await api.get(`/repos/thiagoos16/${repoInput}`);
+        
+            const { name, description, html_url, owner: { avatar_url } } = response.data
+                
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url 
+            });
+
+            this.inputEl.value = '';
+            
+            this.renderRepositories();
+        } catch (error) {
+            alert('Repositório não existe');
+        }
+
+        this.setLoading(false);
     }
 
     renderRepositories() {
