5234bffdf8bcbff4194fa669f083cb041bf5f817
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2020-03-10 15:58:34 -0400

    add setup initial and header component

---
+++ react/huntweb/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./styles.css";
+
+const Header = () => <header id="main-header"> JSHunt </header>
+
+export default Header;
+
+// class Header extends Component {
+//     render() {
+//         return <h1> Hello </h1>
+//     }
+// }
