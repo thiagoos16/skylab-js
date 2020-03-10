329650f5b50da5edbd38af1590aae7e0ce219bfa
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2020-03-10 16:25:24 -0400

    add service add pages component

--- react/huntweb/src/App.js
+++ react/huntweb/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 import Header from './components/Header';
+import Main from './pages/main';
 
 import "./styles.css";
 
 const App = () => (
   <div className="App"> 
     <Header />
+    <Main />
   </div>
 );
 
