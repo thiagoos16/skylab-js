af2b287ad0578c6a13b3e613d0c5655cfe9b76db
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2020-03-10 16:37:22 -0400

    add service to list products in page

--- react/huntweb/src/pages/main/index.js
+++ react/huntweb/src/pages/main/index.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async() => {
+        const response = await api.get('/products');
+        console.log(response);
+    }
+
     render() {
         return <h1> Hello Darkness </h1>
     }
-}
+}src/pages/main/index.js
