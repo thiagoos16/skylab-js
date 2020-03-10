aca87777bb6744647c7752ad0d44f811f62470e7
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2020-03-10 09:47:15 -0400

    add mongoose

--- node/server.js
+++ node/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+    res.send("Hello from darkness!!");
+});
 
 app.listen(3001);
