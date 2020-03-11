083fcdb8f9a7e243b1b103e9378fcd99fc1bf630
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2020-03-11 08:06:10 -0400

    add state manipulation

--- react/huntweb/src/pages/main/index.js
+++ react/huntweb/src/pages/main/index.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+    state = {
+        products: []
+    }
+
     componentDidMount() {
         this.loadProducts();
     }
 
     loadProducts = async() => {
         const response = await api.get('/products');
-        console.log(response);
+        
+        this.setState({ products: response.data.docs });
     }
 
     render() {
-        return <h1> Hello Darkness </h1>
+        return (
+            <div className="product-list">
+                {this.state.products.map(product =>  (
+                    <h2> { product.title } </h2>
+                ))}
+            </div>
+        );
     }
-}src/pages/main/index.js
+}
