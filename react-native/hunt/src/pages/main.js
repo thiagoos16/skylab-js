bd2b17c00ae85448836821b23ca306bd52e59f8e
thiagoos16 <tos@icomp.ufam.edu.br>
2020-03-20 19:36:31 -0400

    add services dependencies

--- react-native/hunt/src/pages/main.js
+++ react-native/hunt/src/pages/main.js
@@ -1,10 +1,23 @@
 import React, { Component }  from 'react';
 import { View, Text } from 'react-native';
+import apu from '../services/api';
 
 export default class Main extends Component {
     static navigationOptions = {
         title: 'JSHunt'
     };
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        const { docs } = response.data;
+
+        console.log(docs);
+    }
     
     render() {
         return (
