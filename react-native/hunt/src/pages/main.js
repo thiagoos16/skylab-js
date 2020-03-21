6f41de6eb7415666172819b91640c7b027f465a3
thiagoos16 <tos@icomp.ufam.edu.br>
2020-03-20 20:33:35 -0400

    add flatlist to products

--- react-native/hunt/src/pages/main.js
+++ react-native/hunt/src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { Component }  from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import api from "../services/api";
 
 export default class Main extends Component {
@@ -22,14 +22,30 @@ export default class Main extends Component {
 
         this.setState({ docs });
     }
+
+    renderItem = ({ item }) => (
+        <View>
+            <Text> {item.title} </Text>
+            <Text> {item.description} </Text>
+            <TouchableOpacity onPress={() => {}}>
+                <Text> Acessar </Text>
+            </TouchableOpacity>
+        </View>
+    )
     
     render() {
         return (
             <View> 
-                <Text> Página Main </Text>
+                {/* <Text> Página Main </Text>
                 { this.state.docs.map(product => (
                     <Text key={product._id}> {product.title} </Text>    
-                ))}
+                ))} */}
+
+                <FlatList
+                    data={this.state.docs}
+                    keyExtractor={item => item.id}
+                    renderItem={this.renderItem}
+                />
             </View>
         );
     }
